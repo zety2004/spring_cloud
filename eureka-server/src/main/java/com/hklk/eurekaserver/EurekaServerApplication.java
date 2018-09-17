4e3f916e0e971d6d6e43e56a37adfddc57bfabb6
caolf <232944973@qq.com>
2018-09-17 14:45:44 +0800

    springCloud微服务1

--- eureka-server/src/main/java/com/hklk/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/hklk/eurekaserver/EurekaServerApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
+
+        System.out.println("springCloud注册中心------start");
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
