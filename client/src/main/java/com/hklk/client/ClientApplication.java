e41543f6e53328f30bc67e9e36202b6e88d64057
caolf <232944973@qq.com>
2018-09-17 14:47:07 +0800

    springCloud微服务2

--- client/src/main/java/com/hklk/client/ClientApplication.java
+++ client/src/main/java/com/hklk/client/ClientApplication.java
@@ -18,6 +18,7 @@ public class ClientApplication {
     }
 
     public static void main(String[] args) {
+        System.out.println("springCloud微服务2------start");
         SpringApplication.run(ClientApplication.class, args);
     }
 }
